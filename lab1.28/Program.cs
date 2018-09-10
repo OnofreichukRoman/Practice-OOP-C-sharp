9b6399c28a9f8401bf4e5b3e0d63953db6addd8c
Roman Onofreichuk <onofreichukroman@gmail.com>
2018-09-11 02:53:07 +0300

    first

---
+++ lab1.28/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1._28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, r;
+
+            Console.WriteLine("Введите х ");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            r = (((2 * x - 3) * x + 4)*x - 5)*x + 6;
+            Console.WriteLine(r);
+            Console.ReadKey();
+            
+            //Схема Горнера
+            }
+          
+        }
+    }
+
