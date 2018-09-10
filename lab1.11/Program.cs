f865326befe3518c1a7e871ac2ef1c9931caacf7
Roman Onofreichuk <onofreichukroman@gmail.com>
2018-09-10 22:11:32 +0300

    First commit

--- lab1.11/Program.cs
+++ lab1.11/Program.cs
@@ -10,6 +10,24 @@ namespace lab1._11
     {
         static void Main(string[] args)
         {
+            double x, y, z, a, b;
+
+            Console.WriteLine("Введите х");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите y");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите z");
+            z = Convert.ToDouble(Console.ReadLine());
+
+            a = y + x / (Math.Pow(y, 2) + Math.Abs(Math.Pow(x, 2) / (y + Math.Pow(x, 3) / 3)));
+            b = 1 + Math.Pow(Math.Tan(z / 2), 2);
+
+            Console.WriteLine("Результат a = {0}", a);
+            Console.WriteLine("Результат b = {0}", b);
+            Console.ReadKey();
+
         }
     }
 }
