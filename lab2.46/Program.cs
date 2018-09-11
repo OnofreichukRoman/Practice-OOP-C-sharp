17c765252634542d2a1aea3cab3a33032912c3e4
Roman Onofreichuk <onofreichukroman@gmail.com>
2018-09-11 20:51:51 +0300

    first

---
+++ lab2.46/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2._46
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+
+            Console.WriteLine("Введите x ");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите y ");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            if (x < 0 && y < 0)
+            {
+                x = Math.Abs(x);
+                y = Math.Abs(y);
+
+            }
+            else if (x < 0 || y < 0)
+            {
+                x = x * 0.5;
+                y = y * 0.5;
+
+            }
+            else if(x >= 0 && y >= 0 && (x < 0.5 && y < 0.5) || (x > 2 && y > 2))
+            {
+                x = x / 10;
+                y = y / 10;
+            }
+
+            Console.WriteLine("Теперь х = {0} у = {1}", x, y);
+            Console.ReadKey();
+        }
+    }
+}
